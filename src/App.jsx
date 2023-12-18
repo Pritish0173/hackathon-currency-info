21bb443615754a515335834a4beeedf30ef00f11
Pritish0173 <pnair.pritish0173@gmail.com>
2023-12-18 23:53:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import data from "./data.json";
+import axios from "axios";
+
+function App() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const startDateFormat = useMemo(
+    () =>
+      `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`,
+    [startDate]
+  );
+  const endDateFormat = useMemo(
+    () => `${endDate.getFullYear()}-${endDate.getMonth()}-${endDate.getDate()}`,
+    [endDate]
+  );
+
+  const options = [
+    { value: "INR", text: "INR" },
+    { value: "US", text: "US" },
+  ];
+  const [currencySelected, setCurrencySelected] = useState(options[0].value);
+
+  const [loading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   axios
+  //     .get("/data.json")
+  //     .then((res) => console.log(res.data))
+  //     .catch((err) => console.log(err));
+  // }, [loading]);
+
+  return (
+    <>
+      <div className="title">Price Index</div>
+      <div className="flex-horizontal">
+        <div>
+          <div>Start Date</div>
+          <div>
+            <DatePicker
+              showIcon
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+            />
+          </div>
+        </div>
+        <div>
+          <div>Start Date</div>
+          <div>
+            <DatePicker
+              showIcon
+              selected={endDate}
+              onChange={(date) => setEndDate(date)}
+            />
+          </div>
+        </div>
+        <div>
+          <div>Select Currency</div>
+          <div>
+            <select
+              className="select-currency"
+              value={currencySelected}
+              onChange={(event) => setCurrencySelected(event.target.value)}
+            >
+              {options.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button>Get Price Index</button>
+      </div>
+      <br />
+      <div className="flex-horizontal-2">
+        <div>
+          <div className="minmax-title">Max Price</div>
+          <div>Date : {data.high.date}</div>
+          <div>Price : {data.high.price}</div>
+        </div>
+        <div>
+          <div className="minmax-title">Min Price</div>
+          <div>Date : {data.low.date}</div>
+          <div>Price : {data.low.price}</div>
+        </div>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(data.bpi).map(([date, price]) => {
+            return (
+              <tr key={date}>
+                <td>{date}</td>
+                <td>{price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
